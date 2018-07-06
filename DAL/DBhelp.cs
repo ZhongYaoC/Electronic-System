ed7ca1cdde76087f97e7cfd2574f757ac99ecc80
Achan Zhong <Achan_zhong@outlook.com>
2018-07-06 16:27:09 +0800

    update

---
+++ DAL/DBhelp.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DBhelp
+    {
+        private static SqlConnection connection;
+
+        //database help class
+        public static SqlConnection GetConnection
+        {
+            get
+            {
+                /*Data Source=DESKTOP-3PF8DBI\SQLEXPRESS;Initial Catalog=ElectronicSystem;User ID=sa;Password=w123456*/
+                //Data Source=LAPTOP-O7FBE1PB\SQLEXPRESS;Initial Catalog=ElectronicSystem;Integrated Security=True
+                string strConn = @"Data Source=LAPTOP-O7FBE1PB\SQLEXPRESS;Initial Catalog=ElectronicSystem;Integrated Security=True";
+                if (connection == null)
+                {
+                    connection = new SqlConnection(strConn);
+                    connection.Open();
+                }
+                else if (connection.State == ConnectionState.Closed)
+                {
+                    connection = new SqlConnection(strConn);
+                    connection.Open();
+                }
+                else if (connection.State == ConnectionState.Broken)
+                {
+                    connection.Close();
+                    connection.Open();
+                }
+                return connection;
+            }
+        }
+
+        //return sqlcommand
+        public static SqlCommand Command(string strSql)
+        {
+            using (SqlCommand cmd = new SqlCommand(strSql, GetConnection))
+            {
+                return cmd;
+            }
+        }
+
+        //return sqldatareader
+        public static SqlDataReader ExecuteReader(string strSql)
+        {
+            SqlCommand cmd = Command(strSql);
+            cmd.CommandType = CommandType.Text;
+            return cmd.ExecuteReader();
+        }
+
+        //update insert delete the database
+        public static int ExecuteNonQuery(string strSql)
+        {
+            SqlCommand cmd = Command(strSql);
+            return cmd.ExecuteNonQuery();
+        }
+
+        //return DataSet
+        public static DataSet DateSet(string strSql)
+        {
+            DataSet ds = new DataSet();
+            SqlDataAdapter adapter = new SqlDataAdapter(strSql, GetConnection);
+            adapter.Fill(ds);
+            return ds;
+        }
+
+        //return DataTable
+        public static DataTable GetDataTable(string strSql)
+        {
+            DataTable dt = new DataTable();
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(strSql, GetConnection);
+            dataAdapter.Fill(dt);
+            return dt;
+        }
+    }
+}
---
+++ database/Sql.sql
@@ -0,0 +1,3 @@
+﻿create database ElectronicSystem
+
+create table 
